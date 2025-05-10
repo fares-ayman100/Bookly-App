0ef9489289051199babd82aeab3a25a7d7370108
Fares Ayman <fareshe73@gmail.com>
2025-05-10 17:53:40 +0300

    create home repo

---
+++ lib/features/home/data/Model/repos/repos.dart
@@ -0,0 +1,8 @@
+import 'package:bookly_app/core/errors/failuer.dart';
+import 'package:bookly_app/features/home/data/Model/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failuer, List<BookModel>>> getBestSellerBook();
+  Future<List<BookModel>> getFeautureBook();
+}

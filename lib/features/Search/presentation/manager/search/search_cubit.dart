505471b5ac72981d95069f0cf24135706949b5ec
Fares Ayman <fareshe73@gmail.com>
2025-05-22 01:32:33 +0300

    search cubit done

---
+++ lib/features/Search/presentation/manager/search/search_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/core/errors/failuer.dart';
+import 'package:bookly_app/features/Search/data/repos/search_repo.dart';
+import 'package:bookly_app/features/home/data/Model/book_model/book_model.dart';
+import 'package:equatable/equatable.dart';
+
+part 'search_state.dart';
+
+class SearchCubit extends Cubit<SearchState> {
+  SearchCubit(this.searchRepo) : super(SearchInitial());
+  final SearchRepo searchRepo;
+  Future<void> fetchSearchBook(String query) async {
+    emit(SearchLoading());
+    final response = await searchRepo.searchBook(query: query);
+    response.fold(
+      (failuer) {
+        emit(SearchFailuer(errMessage: failuer.errMessage));
+      },
+      (books) {
+        emit(SearchSuccess(books: books));
+      },
+    );
+  }
+}
